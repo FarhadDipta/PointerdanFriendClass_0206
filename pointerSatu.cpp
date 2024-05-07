a9202afd01104fe854bc36276ddee04adf3c8b44
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-05-07 14:51:55 +0700

    menyelesaikan class segitga

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class persegiPanjang
 {
 public:
-    int panjang, lebarl;
+    int panjang, lebar;
     int luasPersegiPanjang(int b, int a)
     {
         return a * b;
@@ -13,7 +13,12 @@ public:
 
 class segitiga
 {
-
+public:
+    int alas, tinggi;
+    int luasSegitiga(int a, int t)
+    {
+        return (a * t) / 2;
+    }
 };
 
 int main(){
